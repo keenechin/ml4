965513fe244995066e3789d5783b4d0028ce985c
Keene Chin <keene.chin@gmail.com>
2019-04-08 21:07:45 +0000

    generalized k, proper handdling of pca return (data=>X)

--- gmm_run.py
+++ gmm_run.py
@@ -27,10 +27,9 @@ mu = np.array([[-3,0],[5,0]])
 s1 = np.array([[1,0],[0,1]]).reshape(1,2,2)
 sigma = np.concatenate((s1,s1),axis=0)
 
-data = np.vstack((X,y)).T
-mll = compute_log_likelihood(data)
+mll = compute_log_likelihood(X)
 print('initial mean log-likelihood =  {}'.format(mll)) # Print the mean log-likelihood
-mu10,sigma10,w10,iters10,mll_list10 = gmm(data,mu,sigma,w,10)
+mu10,sigma10,w10,iters10,mll_list10 = gmm(X,mu,sigma,w,10)
 print('\nConverges after {} steps'.format(iters10))
 print('\nAt iteration 10:')
 print('mean log-likelihood = {}'.format(mll_list10[-1]))
@@ -47,8 +46,8 @@ print('w = \n{}'.format(w10)) # Print w at iteration 10
 # YOUR CODE HERE:
 plt.figure()
 for i in range(10):
-    mu,sigma,w = rnd_init(data,2)
-    mu,sigma,w,iters,mll_list = gmm(data,mu,sigma,w,stopping_condition="convergence")
+    mu,sigma,w = rnd_init(X,2)
+    mu,sigma,w,iters,mll_list = gmm(X,mu,sigma,w,stopping_condition="convergence")
     plt.plot(mll_list)
 plt.xlabel('iters')
 plt.ylabel('mean log-likelihood')
@@ -64,9 +63,11 @@ plt.show()
 for k in range(1,6):
     # YOUR CODE HERE:
     plt.figure()
-    mu,sigma,w = rnd_init(data,k)
-    mu,sigma,w,iters,mll_list = gmm(data,mu,sigma,w,stopping_condition="convergence")
+    mu,sigma,w = rnd_init(X,k)
+    mu,sigma,w,iters,mll_list = gmm(X,mu,sigma,w,stopping_condition="convergence")
     plot_ellipse(mu,sigma)
+    sns.scatterplot(X[:,0],X[:,1],hue=np.squeeze(y))
+    plt.title('GMM solution with k = {}'.format(k))
     plt.show()
  
     
